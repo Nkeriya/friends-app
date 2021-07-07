8f873cc394671fdc029f55a94ea8e81d0f325eb6
Neeraj Keriya <nekeriya@bestpeers.com>
2021-07-07 12:53:45 +0530

    Send mail on friend request creation

--- test/mailers/previews/friend_request_mailer_preview.rb
+++ test/mailers/previews/friend_request_mailer_preview.rb
@@ -1,8 +1,3 @@
 # Preview all emails at http://localhost:3000/rails/mailers/friend_request_mailer
 class FriendRequestMailerPreview < ActionMailer::Preview
-  
-  def new_friend_request_email
-    FriendRequestMailer.new_friend_request_email(obj)
-  end
-
 end
