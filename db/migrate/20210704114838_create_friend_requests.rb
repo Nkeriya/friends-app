e1706f316ee5381c2b6ea4cf211665d533a110fe
NkeriyaBestpeers <nekeriya@bestpeers.com>
2021-10-12 15:42:51 +0530

    migrations fixed

--- db/migrate/20210704114838_create_friend_requests.rb
+++ db/migrate/20210704114838_create_friend_requests.rb
@@ -3,8 +3,6 @@ class CreateFriendRequests < ActiveRecord::Migration[5.2]
     create_table :friend_requests do |t|
       t.references :user, index: true, foreign_key: true
       t.references :req_received, index: true
-      t.timestamps null: false
-
       t.timestamps
     end
     add_foreign_key :friend_requests, :users, column: :req_received_id
